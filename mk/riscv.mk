ddcbb8e52875dd4a1c4b5166e6d02aefc02805f7
Yu-En Hsiao <drxiao6399@gmail.com>
2026-03-21 21:18:12 +0800

    Implement dynamic linking for the RISC-V architecture
    This commit primarily improves the ELF handling and code generator to
    enable the compiler to produce a dynamically linked executable targeting
    the RISC-V architecture.
    
    - Update the selected dynamic linker path and the ABI flag.
    - Allow the ELF handling to generate RELA relocation table.
      - Use REL relocation when the target architecture is Arm. Othereise,
        use RELA relocation for RISC-V.
    - Improve GOT generation process.
      - Arm: reserve three entries.
      - RISC-V: reserve two entries.
    - Implement PLT generation for RISC-V.
      - The generation process follows the RISC-V ABI. The first PLT entry
        uses 8 instructions to call '_dl_runtime_resolve'. The subsequent
        entry uses 4 instructions to perform an indirect function call via
        GOT.
    - Refine the function call handling for the RISC-V code generator.
      - Perform a direct call for internal functions
      - Otherwise, use PLT table to peform an indirect call for external
        functions.
    - Enhance the build system:
      - Allow the build system to generate dynamically linked compilers when
        targeting the RISC-V architecture.
      - Detect the sysroot path of the RISC-V GNU toolchain automatically.
    - Remove the unnecessary 'got_offset' field from 'struct func'.

--- mk/riscv.mk
+++ mk/riscv.mk
@@ -1,19 +1,49 @@
 # Enforce the use qemu of by setting the ALLOW_MACHINES variable to empty
 ALLOW_MACHINES =
 ARCH_RUNNER = qemu-riscv32
+
+# Since the selected dynamic linker uses 'double-float ABI',
+# ELF_FLAGS is set to 0x04 here to match this ABI.
 ARCH_DEFS = \
     "/* target: RISCV */\n$\
     \#pragma once\n$\
     \#define ARCH_PREDEFINED \"__riscv\" /* Older versions of the GCC toolchain defined __riscv__ */\n$\
     \#define ELF_MACHINE 0xf3\n$\
-    \#define ELF_FLAGS 0\n$\
-    \#define DYN_LINKER \"/lib/ld-linux.so.3\"\n$\
+    \#define ELF_FLAGS 0x4\n$\
+    \#define DYN_LINKER \"/lib/ld-linux-riscv32-ilp32d.so.1\"\n$\
     \#define LIBC_SO \"libc.so.6\"\n$\
-    \#define PLT_FIXUP_SIZE 20\n$\
-    \#define PLT_ENT_SIZE 12\n$\
+    \#define PLT_FIXUP_SIZE 32\n$\
+    \#define PLT_ENT_SIZE 16\n$\
+    \#define RESERVED_GOT_NUM 2\n$\
     \#define R_ARCH_JUMP_SLOT 0x5\n$\
     \#define MAX_ARGS_IN_REG 8\n$\
     "
 
-# TODO: Set this variable for RISC-V architecture
-RUNNER_LD_PREFIX=
+ifeq ($(USE_QEMU),1)
+    ifeq ($(DYNLINK),1)
+        CROSS_COMPILE = riscv32-unknown-linux-gnu-
+        RISCV_CC = $(CROSS_COMPILE)gcc
+        RISCV_CC := $(shell which $(RISCV_CC))
+        ifndef RISCV_CC
+            $(error "Unable to find RISC-V GNU toolchain.")
+        endif
+
+        LD_LINUX_PATH := $(shell cd $(shell $(RISCV_CC) --print-sysroot) 2>/dev/null && pwd)
+        ifeq ("$(LD_LINUX_PATH)","/")
+            LD_LINUX_PATH := $(shell dirname "$(shell which $(RISCV_CC))")/..
+            LD_LINUX_PATH := $(shell cd $(LD_LINUX_PATH) 2>/dev/null && pwd)
+            LD_LINUX_PATH := $(LD_LINUX_PATH)/$(shell echo $(CROSS_COMPILE) | sed s'/.$$//')/libc
+            LD_LINUX_PATH := $(shell cd $(LD_LINUX_PATH) 2>/dev/null && pwd)
+            ifndef LD_LINUX_PATH
+                LD_LINUX_PATH = /usr/$(shell echo $(CROSS_COMPILE) | sed s'/.$$//')
+                LD_LINUX_PATH := $(shell cd $(LD_LINUX_PATH) 2>/dev/null && pwd)
+            endif
+        endif
+
+        ifndef LD_LINUX_PATH
+            $(error "Dynamic linking mode requires ld-linux.so")
+        endif
+
+        RUNNER_LD_PREFIX = -L $(LD_LINUX_PATH)
+    endif
+endif
